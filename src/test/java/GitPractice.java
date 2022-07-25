d85e4d40d70b261ffdc7627d7fa2e7fade881240
Rupeshh1 <rupesh6658@gmail.com>
2022-07-25 11:52:04 +0530

    my first commit

---
+++ src/test/java/GitPractice.java
@@ -0,0 +1,12 @@
+
+
+public class GitPractice {
+	
+	public void practice1Test1()
+	{
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		System.out.println("----->browser is"+BROWSER+"---->url is"+URL);
+	}
+
+}
